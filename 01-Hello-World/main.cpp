d862763b105d24cd5626e549d43110e0a8c8494f
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-06 12:35:57 +0800

    update 01-Hello-World

---
+++ 01-Hello-World/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// int main(){
+//     std::cout << "Hello World" << std::endl;
+// }
+
+using namespace std;
+int main(){
+    cout << "Hello World" << "!" << endl;
+}
+
+//Note:
+// endl = for end line
+// << for insertion
