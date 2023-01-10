bf4dfae672665363c90ebea9767635a4dfcd932c
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-10 18:06:07 +0800

    add 02-Primitive-Data-Types

---
+++ 02-Primitive-Data-Types/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int Integer = 0;
+    char Character = 'A'; // Use '' instead of ""
+    bool Boolean = true;
+    float Floating = 0.1;
+    double Double = 0.000000001;
+    void function();
+    wchar_t WideCharacter = L'A';
+
+    cout << Integer << "\n" << Character << "\n" << Boolean << "\n" << Floating << "\n" << Double << "\n" << WideCharacter << endl;
+
+    return 0;
+}
