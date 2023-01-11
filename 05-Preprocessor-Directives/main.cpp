1e6c2e7e28cd6825fbb8ae9d018ad69fba786f10
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-11 15:48:43 +0800

    update

---
+++ 05-Preprocessor-Directives/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //example of preprocessor
+#define NUM 3.14 //example of macro
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    printf("Hello, world!\n");
+    cout << NUM << endl;
+}
