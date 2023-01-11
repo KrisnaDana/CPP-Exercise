ff5688bf92977a745e726a7c25cb2d6ee6f824fe
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-11 16:59:02 +0800

    update

---
+++ 07-Keywords/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Keywords in C++ are the collection of reserved words and predefined identifiers. These are written in lower cases and
+    // have a special meaning defined by the compiler. There are 95 keywords in C++, of which around 30 are unavailable in
+    // the C language. These are always used for a particular purpose in a program, but we can’t use them as variable or
+    // function names. Some examples of Keywords in C++ are break, int, void, public, private, auto, return, and many more.
+    
+    return 0;
+}
