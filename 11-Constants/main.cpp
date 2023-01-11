f1428c0f7658112f0bc0c67155ec03d18c954eb0
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-11 17:25:15 +0800

    update

---
+++ 11-Constants/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define y 10 // define
+
+using namespace std;
+
+int main(){
+    // There are two ways to declare a constants
+
+    const int x = 1; // const variable
+
+
+    return 0;
+}
