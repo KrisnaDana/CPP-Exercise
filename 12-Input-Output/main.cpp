87eb32320a43e08726a4029b4da6011df508ef37
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-11 17:31:53 +0800

    update

---
+++ 12-Input-Output/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string x;
+    int y;
+
+    cout << "Your string input: ";
+    cin >> x;
+
+    cout << "Your int input: ";
+    cin >> y;
+
+    cout << x << endl;
+    cout << y << endl;
+}
