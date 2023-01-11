4715b1f6486eaa80bf80b9b9180922f46342d3bc
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-11 17:02:19 +0800

    update

---
+++ 08-Identifiers/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    /*Identifiers in C++ are a way of naming things. They are names for variables, functions, classes, and more.
+    The number of unique identifiers is essential because it limits the scope of a variable, for example.
+    Identifiers follow specific rules and provide some protection from mistakes.*/
+
+    int identifier_name = 0;
+    cout << identifier_name << endl;
+
+    return 0;
+}
