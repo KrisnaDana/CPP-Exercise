4ba361650b3e750c29f2f2a1288a01cb6b664d41
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-11 17:05:40 +0800

    update

---
+++ 09-Main-Function/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /*The main() function is the central entry point to a C++ program. It has two main jobs. First, it starts up the program’s execution and handles basic setup.
+    Second, it provides a place to put functions that perform I/O or manage memory, handling any programs that will run simultaneously*/
+
+    return 0;
+}
