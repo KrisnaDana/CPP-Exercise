30988af64c1b61fbfabd54b6b70e4191290fc1ea
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-13 11:00:35 +0800

    update

---
+++ 27-Do-While-Loop/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    while(true){
+        cout << "Welcome" << endl;
+        break;
+    }
+
+    do{
+        cout << "Goodbye" << endl;
+    }while(false);
+
+    return 0;
+}
