ed5d0b1580be6b8b999dc630545d5c6b520cef61
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-13 10:40:18 +0800

    update

---
+++ 23-Break/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    while(true){
+        cout << "Welcome" << endl;
+        if(true){
+            break;
+        }
+    }
+
+    return 0;
+}
