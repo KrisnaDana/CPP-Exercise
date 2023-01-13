febe03c16a97f6417ee702a7f92e430f92dd49b6
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-13 09:50:53 +0800

    update

--- 20-Multidimensional-Arrays/main.cpp
+++ 20-Multidimensional-Arrays/main.cpp
@@ -15,6 +15,27 @@ int main(){
             cout << arr[i][j] << endl;
         }
     }
+
+    int arr2[2][2][2] = {
+        {
+            {1, 2},
+            {3, 4},
+        },
+        {
+            {5, 6},
+            {7, 8}
+        }
+    };
+
+    cout << endl;
+
+    for(int i=0; i<2; i++){
+        for(int j = 0; j<2; j++){
+            for(int k = 0; k<2; k++){
+                cout << arr2[i][j][k] << endl;
+            }
+        }
+    }
     
     return 0;
 }
