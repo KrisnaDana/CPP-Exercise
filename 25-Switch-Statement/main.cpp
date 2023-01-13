dc7072a30f69ccae39e04ea220dfb39207a68222
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-13 10:48:46 +0800

    update

---
+++ 25-Switch-Statement/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x = 1;
+
+    switch(x){
+        case 0:
+            cout << 0 << endl;
+            break;
+        case 1:
+            cout << 1 << endl;
+            break;
+        default:
+            break;
+    }
+
+    return 0;
+}
