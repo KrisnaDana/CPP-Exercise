c05bcc9ff2eb9a0c3de1c297febb81ce00841bae
Krisna Dana <krisnadanaofficial@gmail.com>
2023-01-14 17:17:19 +0800

    update

---
+++ 30-Default-Argument/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void display(char = '*', int = 10);
+// or this void display(char = '*', int = 10);
+
+
+int main(){
+
+    display();
+    display('#', 1);
+
+    return 0;
+}
+
+void display(char x, int y){
+    cout << x << " " << y << endl;
+}
